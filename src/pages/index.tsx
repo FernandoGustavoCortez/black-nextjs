e84b47a70b6807777bf7d64df425f075c615b66c
Fernando Gustavo Cortez <gustavomora778@gmail.com>
2024-02-07 11:31:05 -0300

    Teste
    Teste Vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
 
       <main>
         <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome tooo <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p>
