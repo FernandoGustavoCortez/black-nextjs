06499119738cb343d5530542430cea810f7db797
Fernando Gustavo Cortez <gustavomora778@gmail.com>
2024-02-08 10:53:49 -0300

    Aula 09
    Aula 09

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,7 +1,11 @@
 // pages/cart.tsx
 
+import CartTotal from "@/components/CardTotal";
+import CartTable from "@/components/CartTable";
+import Header from "@/components/Header";
 import { NextPage } from "next";
 import Head from "next/head";
+import { Container } from "reactstrap";
 
 const Cart: NextPage = () => {
   return (
@@ -11,12 +15,18 @@ const Cart: NextPage = () => {
         <meta name="description" content="Meu carrinho de compras" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
+      <h1>Carrinho</h1>
+      <main>
+        <Container className="mb-5">
+          <h1 className="my-5">Carrinho</h1>
 
-      <h1>
-        Carrinho
-      </h1>
+          <CartTable />
+          <CartTotal />
+        </Container>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
